c37e5b932977af04b1e1c8d481dc34c8a6b612b0
Artimedias <brettroper22@gmail.com>
2022-05-08 00:08:30 -0700

    Finally got home from work and got test 1 done

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
+//business
 
+function pizza(size, sauce, toppings, dressing) {
+    this.size = size;
+    this.sauce = sauce;
+    this.toppings = toppings;
+    this.dressing = dressing;
+  }
+
+  pizzaOne = new pizza("large", "red", ["Mozzerella" , "Black Olives", "Pepperoni"], "Hot Honey");
+  pizzaOne.toppings.forEach(function(element) {
+    pizzaString = pizzaString.concat(element); 
+  });
+
+console.log(pizzaString);
 //user
 
 $(document).ready(function() {
